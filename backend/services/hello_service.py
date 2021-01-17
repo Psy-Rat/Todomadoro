4467e217f91ff0d6745b9f9e1254c58bfa0dd754
Psy-Rat <micekitty@ya.ru>
2021-01-17 18:55:16 +0300

    Исправление путей

--- backend/services/hello_service.py
+++ backend/services/hello_service.py
@@ -1,10 +1,14 @@
 from flask import jsonify
 import json
+import os
+
+# BACKEND_SERVICES = "\\backend\\services"
 
 def hello(name : str):
     return jsonify({ 'msg': f'hello, {name}'})
 
 def fakeDataApI():
-    path = 'D:\\FILES\Desktop\\todomodoro4\\backend\\services\\data.json'
+    __wdir__ = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(__wdir__, "data.json")
     data = json.load(open(path))
     return jsonify(data)
